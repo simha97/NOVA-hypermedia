9bcafe1fa4fb3f8649853474f3f29344368e609f
simon hallak <simon.hallak.3@gmail.com>
2023-06-02 12:54:44 +0200

    fixed the picture

--- pages/dogs/[id].vue
+++ pages/dogs/[id].vue
@@ -5,7 +5,7 @@
 <template>
     <main>
         <div class = "info-group">
-            <img id = "main-img" src = "~/assets/img/home-image.jpg" />
+            <img id = "main-img" src = "~/assets/img/youngwoman.png" />
             <div id = "data-container">
                 <p class = "data">Name: <span>{{ person.name }}</span></p>
                 <p class = "data">Surname: <span>{{ person.surname }}</span></p>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 -->
 <template>
     <main>
-        <img id = "main-img" src = "~/assets/img/home-image.jpg" />
+        <img id = "main-img" src = "~/assets/img/youngwoman.png" />
         <h2>
             This is a description for the Dog's Hotel. The best place where dogs can rest.
         </h2>
