af026d09671241fc14ac5bec70e0a040841f2e88
Daniele Cicala <10630561@polimi.it>
2023-06-08 15:39:11 +0200

    change name of logos for easier binding. Projects is now almost done.

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -15,12 +15,12 @@
   <div class="flex-container">
     <NuxtLink :to = "link">
       <div class="project-card">
-        <img class="main-img" alt="Avatar" style="width:100%" src="~\LogoPng\01HealthyBites.png">
+        <img class="main-img" alt="logo" style="width:100%" v-bind:src="'/_nuxt/LogoPng/' + title + '.png'">
         <div class="data-container">
           <b><div style="font-size: 16px;">{{ title }}</div></b>
           <div style="font-size: 12px;">{{ mainIdea }}</div>
-          <div style="font-size: 12px;">{{ city }}</div>
-          <div style="font-size: 12px; text-align: right;">{{ area }}</div>
+          <div style="font-size: 12px; position: absolute; bottom: 0">{{ city }}</div>
+          <div style="font-size: 12px; text-align: right; ">{{ area }}</div>
         </div>
       </div>
     </NuxtLink>
@@ -28,43 +28,45 @@
 </template>
 
 <script setup>
-  const props = defineProps(['logo', 'title', 'mainIdea', 'city', 'area', 'link'])
+  const props = defineProps(['title', 'mainIdea', 'city', 'area', 'link']);
+
 </script>
 
 <style>
 
-.flex-container {
-  padding:  32px;
-}
+  .flex-container {
+    padding:  8px;
+  }
 
-.project-card {
-/* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  width: 256px;
-  height: 400px;
-  background-color: white;
-  color: #282E36;
-}
+  .project-card {
+  /* Add shadows to create the "card" effect */
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 240px;
+    height: 400px;
+    background-color: white;
+    color: #282E36;
+  }
 
-.project-card:hover{
-  background-color: #282E36;
-  color:white;
-}
+  .project-card:hover{
+    background-color: #282E36;
+    color:white;
+  }
 
-/*
-.project-card .main-img{
-  filter: grayscale(100%); transition: filter .2s ease-in-out;
-}
-.project-card .main-img:hover{
-  filter: none;
-}
-*/
+  /*
+  .project-card .main-img{
+    filter: grayscale(100%); transition: filter .2s ease-in-out;
+  }
+  .project-card .main-img:hover{
+    filter: none;
+  }
+  */
 
 
-/* Add some padding inside the card container */
-.data-container {
-  padding: 2px 16px;
-}
+  /* Add some padding inside the card container */
+  .data-container {
+    padding: 2px 16px;
+    position: relative;
+  }
 
 </style>
