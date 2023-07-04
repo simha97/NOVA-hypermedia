a3edab60c7e5bccbff58ec0e96225a46e5ecef93
Daniele Sinigaglia <10574224@polimi.it>
2023-07-04 12:13:43 +0200

    all areas, component areacard, api/areas/index

---
+++ components/AreaCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div><NuxtLink :to="'/areas/'+name">{{ name }}</NuxtLink></div>
+</template>
+
+<style scoped>
+
+    div.area-button{
+        border: 1px solid #F26225;
+        background: #FFFFFF;
+        border-radius: 8px;
+        width: 176px;
+        height: 64px;
+        transition: box-shadow .3s ease-in-out;
+        margin-top: 240px;
+    }
+</style>
+
+<script setup>
+    const props = defineProps(['name']);
+</script>
+
