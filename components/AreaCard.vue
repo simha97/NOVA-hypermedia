2da7d5aefa736232fd49e142d8ceb9c60e2f50da
Daniele Cicala <10630561@polimi.it>
2023-07-08 15:56:48 +0200

    area card hover effect changed

--- components/AreaCard.vue
+++ components/AreaCard.vue
@@ -1,17 +1,18 @@
 <template>
-    <NuxtLink :to="'/areas/' + id">
-        <div class="area-button" v-bind:style="{ backgroundImage: 'url(' + img + ')'}">
-            <img class="shadow" src="~\assets\img\bigProjectHoverShadow.png">
-            <div class="data-container">
-                <div class="area-name">
-                    #{{name}}
-                </div>
+    <div class="area-button" v-bind:style="{ backgroundImage: 'url(' + img + ')'}">
+        <img class="shadow" src="~\assets\img\bigProjectHoverShadow.png">
+        <div class="data-container">
+            <div class="area-name">
+                #{{name}}
+            </div>
+            <div class="hidden">
                 <div class="area-description">
                     {{description}}
                 </div>
+                <NuxtLink :to = link>Show me more</NuxtLink>
             </div>
         </div>
-    </NuxtLink>
+    </div>
 </template>
 
 <style scoped>
@@ -37,6 +38,11 @@
         font-size: 24px;
         font-weight: bold;
     }
+    
+    .area-name{
+        font-size: 24px;
+        font-weight: bold;
+    }
 
     .area-button:hover .data-container{
         margin-top: 32px;
@@ -53,12 +59,14 @@
         opacity: 100%;
     }
 
-    .area-description{
+    .hidden{
         font-size: 24px;
-        font-weight: regular;
         margin-top: 48px;
         opacity: 0;
         transition: opacity .3s ease;
+    }
+
+    .hidden .area-description{
         max-width: 480px;
         display: -webkit-box;
         -webkit-line-clamp: 4;
@@ -66,16 +74,32 @@
         overflow: hidden;
     }
 
-    .area-button:hover .area-description{
+    .area-button:hover .hidden{
         opacity: 100%;
         transition: opacity .9s ease-in;
+        pointer-events: auto;
     }
+
+    .hidden a{
+        font-family: "Myriad-Pro-Bold";
+        color: #F26225;
+        font-style: italic;
+        position: absolute;
+        margin-top: 16px;
+        margin-left: 320px;
+    }
+
+    .area-button:hover .hidden a:hover{
+        color: #F26225;
+        text-decoration: underline;
+    }
+
 
 </style>
 
 <script setup>
 
-    const props = defineProps(['id', 'name', 'img', 'description']);
+    const props = defineProps(['id', 'name', 'img', 'description', 'link']);
 
 </script>
 
